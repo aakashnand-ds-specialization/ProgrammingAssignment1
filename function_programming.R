e97b63a1022fca95aca3a6f994696417f2e2961b
Aakash Nand <aakashnand@nttv6.jp>
2019-03-28 19:31:24 +0900

    started R programming script

---
+++ function_programming.R
@@ -0,0 +1,11 @@
+# Author: Aakash Nand
+# Coursera: Week-2 R-programming function programming assignment
+# Date: 28/03/2019
+
+pollutantmean<-function(directory,pollutant,id=1:332){
+  # for number of ids {
+  #   read csv file from directory
+  #   append the pollutant values to pollutant_vector
+  # }
+  # mean=mean of pollutant_vector  
+}
