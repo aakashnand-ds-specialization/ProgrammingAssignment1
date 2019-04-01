cd53aea6e308a814f39a6dec4e881d5c22423aaa
Aakash Nand <aakashnand@nttv6.jp>
2019-04-01 20:14:25 +0900

    completed 2nd function named complete

--- function_programming.R
+++ function_programming.R
@@ -18,3 +18,31 @@ pollutantmean<-function(directory,pollutant,id=1:332){
   result<-mean(mean_vector)
   result
 }
+
+complete <-function(directory, id=1:332){
+  
+  if(grep('specdata',directory)==1){
+    directory<-("./specdata/")
+  }
+  all_files<-as.character(list.files(directory))
+  file_paths<-paste(directory,all_files,sep="")
+  
+  # Initialize empty dataframe
+  result_df<-data.frame(id=id,nobs=numeric(length(id)))
+  
+  
+  for (i in id){
+    count=0
+    file<-read.table(file_paths[i],header = T, sep=",")
+    
+    for (index in 1:nrow(file)){
+      if(!is.na(file[index,2]) && !is.na(file[index,3])){
+         count<-count+1
+      }
+      result_df[ which(result_df$id==i),2 ] <- count
+    }
+  }
+  
+  print(result_df)
+  
+}
